4d37df99b767130ab521633292a8dfe8b185bf60
Tanmay Sharma <tanmay7575sharma@gmail.com>
2026-06-02 23:59:13 +0530

    attached queue to engine

--- backend/engine/src/index.ts
+++ backend/engine/src/index.ts
@@ -1,19 +1,23 @@
 import { RedisClientType } from "@redis/client";
 import { createClient } from "redis";
+import { Engine } from "./trade/Engine";
 
 async function main(){
-const engine = "engine";
+const engine = new Engine();
 const client:RedisClientType = createClient();
 await client.connect();
 while(true)
 {
-    const res = client.rPop("messages" as string);
+    const res = await client.rPop("messages" as string);
     if(!res)
     {
 
     }
     else
     {
+        const {message, clientId} = JSON.parse(res);
+        engine.process({message, clientId});
+
         //push to engine
     }
 }
